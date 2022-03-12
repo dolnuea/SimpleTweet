ad942ea4e4f3255976bfb2978089e27ac4719dcf
Luna Dagci <ldagci@albany.edu>
2022-03-12 00:40:36 -0500

    Refresh

--- app/src/main/java/com/codepath/apps/SimpleTweet/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/SimpleTweet/TimelineActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.codepath.apps.SimpleTweet.models.Tweet
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
@@ -12,6 +13,7 @@ import org.json.JSONException
 
 class TimelineActivity : AppCompatActivity() {
 
+    lateinit var swipeContainer: SwipeRefreshLayout
     lateinit var client: TwitterClient
     lateinit var rvTweets: RecyclerView
     lateinit var adapter: TweetsAdapter
@@ -19,6 +21,24 @@ class TimelineActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timeline)
+
+        // Lookup the swipe container view
+        swipeContainer = findViewById(R.id.swipeContainer)
+
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener {
+            Log.i(TAG, "Refresh listener")
+            // Your code to refresh the list here.
+            populateHomeTimeline()
+            // Make sure you call swipeContainer.setRefreshing(false)
+            // once the network request has completed successfully.
+        }
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+            android.R.color.holo_green_light,
+            android.R.color.holo_orange_light,
+            android.R.color.holo_red_light);
 
         client = TwitterApplication.getRestClient(this)
         rvTweets = findViewById(R.id.rvTweets)
@@ -46,9 +66,16 @@ class TimelineActivity : AppCompatActivity() {
 
                 val JSONArray = json.jsonArray
                 try {
+
+                    // Remember to CLEAR OUT old items before appending in the new ones
+                    adapter.clear()
+
                     val listOfNewTweetsRetrieved = Tweet.fromJsonArray(JSONArray)
                     tweets.addAll(listOfNewTweetsRetrieved)
                     adapter.notifyDataSetChanged()
+
+                    // Now we call setRefreshing(false) to signal refresh has finished
+                    swipeContainer.isRefreshing = false
                 } catch (e: JSONException){
                     Log.e(TAG, "JSON Exception $e")
                 }
