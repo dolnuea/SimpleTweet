c9f56c3eef95aa5f988de60c6988f35cc5e4061c
Luna Dagci <ldagci@albany.edu>
2022-03-19 00:22:47 -0400

    See character count

--- app/src/main/java/com/codepath/apps/SimpleTweet/ComposeActivity.kt
+++ app/src/main/java/com/codepath/apps/SimpleTweet/ComposeActivity.kt
@@ -3,9 +3,12 @@ package com.codepath.apps.SimpleTweet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 import com.codepath.apps.SimpleTweet.models.Tweet
@@ -15,16 +18,49 @@ import okhttp3.Headers
 class ComposeActivity : AppCompatActivity() {
 
     lateinit var etCompose: EditText
+    lateinit var characterCount: TextView
     lateinit var btnTweet: Button
     lateinit var client: TwitterClient
+    var charactersLeft: Int = MAX
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compose)
 
         etCompose = findViewById(R.id.etTweetCompose)
+        characterCount = findViewById(R.id.tvCharCount)
         btnTweet = findViewById(R.id.btnTweet)
         client = TwitterApplication.getRestClient(this)
+
+        characterCount.text = "$MAX characters left"
+
+        //handle character count
+        etCompose.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //todo
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //todo
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s != null) {
+                    charactersLeft = charactersLeft(s.length)
+                }
+                characterCount.text = "$charactersLeft characters left"
+
+                if (charactersLeft < 0){
+                    characterCount.setTextColor(resources.getColor(R.color.red))
+                    btnTweet.isClickable = false
+                }
+                else {
+                    characterCount.setTextColor(resources.getColor(R.color.black))
+                    btnTweet.isClickable = true
+                    }
+            }
+
+        })
 
         // handle user click on tweet button
         btnTweet.setOnClickListener {
@@ -36,8 +72,8 @@ class ComposeActivity : AppCompatActivity() {
                 Toast.makeText(this, "Please write for tweet", LENGTH_SHORT).show()
             }
             //make sure tweet is under char count
-            else if (tweetContent.length > 140) {
-                Toast.makeText(this, "Allowed character count is 140 characters", LENGTH_SHORT).show()
+            else if (tweetContent.length > 280) {
+                Toast.makeText(this, "Allowed character count is 280 characters", LENGTH_SHORT).show()
             }
             else {
                 // make api call to publish tweet
@@ -67,7 +103,13 @@ class ComposeActivity : AppCompatActivity() {
 
         }
     }
+
+    fun charactersLeft(count: Int): Int {
+        return MAX - count
+    }
+
     companion object {
         val TAG = "ComposeTweet"
+        val MAX = 280
     }
 }
